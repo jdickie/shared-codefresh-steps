3c68a4ad85173b970e74d6c48a608e2f3fb2edb7
jdickie <gdickie@npr.org>
2020-03-12 08:41:23 -0400

    Adding in initial project, ssm-to-yaml

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shared Codefresh Steps
+
+Collection of steps I've been working on that can be of use to others.
---
+++ incubating/ssm-to-yaml/convert.js
@@ -0,0 +1,32 @@
+const yaml = require('js-yaml');
+const aws = require('aws-sdk');
+const fs   = require('fs');
+
+const SSM_PATH = process.env.SSM_PATH;
+const YAML_OUTPUT_PATH = process.env.YAML_OUTPUT_PATH;
+
+const getSSMValues = (path) => {
+    const ssm = new aws.SSM();
+    ssm.getParametersByPath({
+        Path: path,
+        Recursive: true,
+        WithDecryption: false
+    }, (err, data) => {
+        if (err) {
+            console.log('Error retrieving SSM data');
+            console.log(err.message);
+        }
+        const output = {
+            secrets: []
+        };
+        data.Parameters.forEach((el) => {
+            output.secrets.push({
+                name: el.Name.replace(SSM_PATH, '').replace('/', ''),
+                value: el.Value
+            });
+        });
+        fs.writeFileSync(YAML_OUTPUT_PATH, yaml.safeDump(output));
+    });
+}
+
+getSSMValues(SSM_PATH);
