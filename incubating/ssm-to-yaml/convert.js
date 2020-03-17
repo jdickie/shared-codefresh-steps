8f62aa9b7d582ab84e8d445c188c16318218b598
jdickie <gdickie@npr.org>
2020-03-17 10:31:09 -0400

    feat: Upgrading node aws to loop through params; wasn't getting all of them out

--- incubating/ssm-to-yaml/convert.js
+++ incubating/ssm-to-yaml/convert.js
@@ -5,30 +5,46 @@ const fs   = require('fs');
 const SSM_PATH = process.env.SSM_PATH;
 const YAML_OUTPUT_PATH = process.env.YAML_OUTPUT_PATH;
 
-const getSSMValues = (path) => {
+const getSSMValues = async (path, token = null) => {
     const ssm = new aws.SSM();
-    ssm.getParametersByPath({
+    const options = {
         Path: path,
         Recursive: true,
         WithDecryption: false
-    }, (err, data) => {
-        if (err) {
-            console.log('Error retrieving SSM data');
-            console.log(err.message);
+    };
+    if (token) {
+        options.NextToken = token;
+    }
+    // Gets values on path or starts at NextToken. We only get back 10 results max even when there is
+    // a MaxToken set in options - so leaving that default.
+    const results = await ssm.getParametersByPath(options).promise();
+    
+    return results;
+}
+
+const getAllValues = async () => {
+    let data = {};
+
+    const output = {
+        ssm: {
+            secrets: []
         }
-        const output = {
-            ssm: {
-                secrets: []
-            }
-        };
+    };
+   
+    let token = 0;
+    data = await getSSMValues(SSM_PATH);
+    // Need to supply a NextToken - as long as this is present we're supposed to loop through results.
+    while (data.NextToken) {
         data.Parameters.forEach((el) => {
             output.ssm.secrets.push({
                 name: el.Name.replace(SSM_PATH, '').replace('/', ''),
                 value: el.Value
             });
         });
-        fs.writeFileSync(YAML_OUTPUT_PATH, yaml.safeDump(output), { flag: 'wx' });
-    });
+        token = data.NextToken;
+        data = await getSSMValues(SSM_PATH, token);
+    }
+    fs.writeFileSync(YAML_OUTPUT_PATH, yaml.safeDump(output), { flag: 'wx' });
 }
 
-getSSMValues(SSM_PATH);
+getAllValues();
